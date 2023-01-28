2b86e38f67eac0539fcb2e47276cc1ff4e7a0d6c
SpecialistSteak <102715674+SpecialistSteak@users.noreply.github.com>
2023-01-28 20:20:00 +0530

    fixed bugs

--- src/main/java/specialiststeak/TimeUtils/TimedCode.java
+++ src/main/java/specialiststeak/TimeUtils/TimedCode.java
@@ -1,27 +1,11 @@
 package specialiststeak.TimeUtils;
 
 public interface TimedCode {
-    /**
-     * Runs a piece of code and returns the time it took to run in nanoseconds
-     *
-     * @param code The code to run
-     * @return The time it took to run in nanoseconds
-     */
-    public static long runCode(Runnable code) {
-        long startTime = System.nanoTime();
-        code.run();
-        long endTime = System.nanoTime();
-        return (endTime - startTime);
+    static long runCode(Runnable code) {
+        return 0;
     }
 
-    /**
-     * Runs a piece of code and adds the time it took to run to the TimeTester object
-     *
-     * @param code The code to run
-     */
-    public static void timerRunCode(Runnable code) {
-        long startTime = System.nanoTime();
-        code.run();
-        long endTime = System.nanoTime();
+    static void timerRunCode(Runnable code){
+        return;
     }
 }
