71d43424e8a67dd40afbc1ed909b7dcd8d2f958a
SpecialistSteak <102715674+SpecialistSteak@users.noreply.github.com>
2023-02-16 11:04:47 +0530

    added javadoc comments

--- src/main/java/specialiststeak/Algorithms/SortAlgorithms.java
+++ src/main/java/specialiststeak/Algorithms/SortAlgorithms.java
@@ -10,32 +10,38 @@ import static specialiststeak.Algorithms.SortTimeTester.runCode;
 
 public class SortAlgorithms {
     private static final String[] sortNames = {
-        "Arrays.sort", "Bubble Sort", "Bubble Sort Recursive", "Circle Sort",
-        "Cocktail Shaker Sort", "Comb Sort", "Counting Sort",
-        "Cycle Sort", "Dual Pivot Quick Sort", "Dutch National Flag Sort",
-        "Gnome Sort", "Hash Map Sort", "Heap Sort",
-        "Insertion Sort", "Introspective Sort", "Lame Sort",
-        "Merge Sort", "Pancake Sort", "Quick Sort",
-        "Selection Sort", "Shell Sort", "Simple Sort",
-        "Swap Sort", "Tim Sort", "Tree Sort",
-        "Wiggle Sort"
+            "Arrays.sort", "Bubble Sort", "Bubble Sort Recursive", "Circle Sort",
+            "Cocktail Shaker Sort", "Comb Sort", "Counting Sort",
+            "Cycle Sort", "Dual Pivot Quick Sort", "Dutch National Flag Sort",
+            "Gnome Sort", "Hash Map Sort", "Heap Sort",
+            "Insertion Sort", "Introspective Sort", "Lame Sort",
+            "Merge Sort", "Pancake Sort", "Quick Sort",
+            "Selection Sort", "Shell Sort", "Simple Sort",
+            "Swap Sort", "Tim Sort", "Tree Sort",
+            "Wiggle Sort"
     };
 
+    /**
+     * A method to test all the sorting algorithms real execution time, and rank them from fastest to slowest.
+     *
+     * @param array The array to sort
+     * @param <T>  The type of the array. It must be comparable.
+     */
     public static <T extends Comparable<T>> void testAll(T[] array) {
         long[] arr = new long[sortNames.length];
         for (int i = 0; i < sortNames.length; i++) {
             shuffle(array);
             switch (i) {
-                case 0 -> arr[0]   = runCode(() -> new ArraysSort().sort(array));
-                case 1 -> arr[1]   = runCode(() -> new BubbleSort().sort(array));
-                case 2 -> arr[2]   = runCode(() -> new BubbleSortRecursion().sort(array));
-                case 3 -> arr[3]   = runCode(() -> new CircleSort().sort(array));
-                case 4 -> arr[4]   = runCode(() -> new CocktailShakerSort().sort(array));
-                case 5 -> arr[5]   = runCode(() -> new CombSort().sort(array));
-                case 6 -> arr[6]   = runCode(() -> new CountingSort().sort(array));
-                case 7 -> arr[7]   = runCode(() -> new CycleSort().sort(array));
-                case 8 -> arr[8]   = runCode(() -> new DualPivotQuickSort().sort(array));
-                case 9 -> arr[9]   = runCode(() -> new DutchNationalFlagSort().sort(array));
+                case 0 -> arr[0] = runCode(() -> new ArraysSort().sort(array));
+                case 1 -> arr[1] = runCode(() -> new BubbleSort().sort(array));
+                case 2 -> arr[2] = runCode(() -> new BubbleSortRecursion().sort(array));
+                case 3 -> arr[3] = runCode(() -> new CircleSort().sort(array));
+                case 4 -> arr[4] = runCode(() -> new CocktailShakerSort().sort(array));
+                case 5 -> arr[5] = runCode(() -> new CombSort().sort(array));
+                case 6 -> arr[6] = runCode(() -> new CountingSort().sort(array));
+                case 7 -> arr[7] = runCode(() -> new CycleSort().sort(array));
+                case 8 -> arr[8] = runCode(() -> new DualPivotQuickSort().sort(array));
+                case 9 -> arr[9] = runCode(() -> new DutchNationalFlagSort().sort(array));
                 case 10 -> arr[10] = runCode(() -> new GnomeSort().sort(array));
                 case 11 -> arr[11] = runCode(() -> new HashMapSort().sort(array));
                 case 12 -> arr[12] = runCode(() -> new HeapSort().sort(array));
@@ -57,6 +63,15 @@ public class SortAlgorithms {
         printResults(arr);
     }
 
+    /**
+     * A method to test all the sorting algorithms real execution time, and rank them from fastest to slowest.
+     * This is different from the other testAll method as it runs n times and averages the runtime, leading to more
+     * accurate results.
+     *
+     * @param array The array to sort
+     * @param iterations The number of times to run the test
+     * @param <T> The type of the array. It must be comparable.
+     */
     public static <T extends Comparable<T>> void testAll(T[] array, int iterations) {
         long[][] ar2r = new long[sortNames.length][iterations];
         long[] average = new long[sortNames.length];
@@ -65,16 +80,16 @@ public class SortAlgorithms {
             for (int j = 0; j < iterations; j++) {
                 shuffle(array);
                 switch (i) {
-                    case 0 -> ar2r[0][j]   = runCode(() -> new ArraysSort().sort(array));
-                    case 1 -> ar2r[1][j]   = runCode(() -> new BubbleSort().sort(array));
-                    case 2 -> ar2r[2][j]   = runCode(() -> new BubbleSortRecursion().sort(array));
-                    case 3 -> ar2r[3][j]   = runCode(() -> new CircleSort().sort(array));
-                    case 4 -> ar2r[4][j]   = runCode(() -> new CocktailShakerSort().sort(array));
-                    case 5 -> ar2r[5][j]   = runCode(() -> new CombSort().sort(array));
-                    case 6 -> ar2r[6][j]   = runCode(() -> new CountingSort().sort(array));
-                    case 7 -> ar2r[7][j]   = runCode(() -> new CycleSort().sort(array));
-                    case 8 -> ar2r[8][j]   = runCode(() -> new DualPivotQuickSort().sort(array));
-                    case 9 -> ar2r[9][j]   = runCode(() -> new DutchNationalFlagSort().sort(array));
+                    case 0 -> ar2r[0][j] = runCode(() -> new ArraysSort().sort(array));
+                    case 1 -> ar2r[1][j] = runCode(() -> new BubbleSort().sort(array));
+                    case 2 -> ar2r[2][j] = runCode(() -> new BubbleSortRecursion().sort(array));
+                    case 3 -> ar2r[3][j] = runCode(() -> new CircleSort().sort(array));
+                    case 4 -> ar2r[4][j] = runCode(() -> new CocktailShakerSort().sort(array));
+                    case 5 -> ar2r[5][j] = runCode(() -> new CombSort().sort(array));
+                    case 6 -> ar2r[6][j] = runCode(() -> new CountingSort().sort(array));
+                    case 7 -> ar2r[7][j] = runCode(() -> new CycleSort().sort(array));
+                    case 8 -> ar2r[8][j] = runCode(() -> new DualPivotQuickSort().sort(array));
+                    case 9 -> ar2r[9][j] = runCode(() -> new DutchNationalFlagSort().sort(array));
                     case 10 -> ar2r[10][j] = runCode(() -> new GnomeSort().sort(array));
                     case 11 -> ar2r[11][j] = runCode(() -> new HashMapSort().sort(array));
                     case 12 -> ar2r[12][j] = runCode(() -> new HeapSort().sort(array));
@@ -104,6 +119,11 @@ public class SortAlgorithms {
         printResults(average);
     }
 
+    /**
+     * Method to print the results of the testAll method.
+     *
+     * @param arr The array of times to print
+     */
     private static void printResults(long[] arr) {
         SortTimeTester timeTester = new SortTimeTester();
         for (int i = 0; i < sortNames.length; i++) {
